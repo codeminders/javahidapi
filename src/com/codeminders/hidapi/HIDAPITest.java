00af1c0cdd5e6264aa9af9096fc54354044236e0
vitaliy volokh <volokh@codeminders.com>
2011-12-23 05:34:32 +0200

    Added the hid manager test for when device connected/disconnected

--- src/com/codeminders/hidapi/HIDAPITest.java
+++ src/com/codeminders/hidapi/HIDAPITest.java
@@ -2,6 +2,11 @@ package com.codeminders.hidapi;
 
 import java.io.IOException;
 
+
+/**
+ * This class demonstrates enumeration, reading and getting
+ * notifications when a HID device is connected/disconnected.
+ */
 public class HIDAPITest
 {
     private static final long READ_UPDATE_DELAY_MS = 50L;
@@ -14,23 +19,49 @@ public class HIDAPITest
     // "Afterglow" controller for PS3
     static final int VENDOR_ID = 3695;
     static final int PRODUCT_ID = 25346;
-    private static final int BUFSIZE = 2048; 
+    private static final int BUFSIZE = 2048;
+    private static final int MAXOBJECTS = 100;
+        
+    /**
+     * @param args input strings value.
+     */
+    public static void main(String[] args) throws IOException
+    {
+        try
+        {
+            HIDManager test;
+            test  = new HIDManagerTest();
+            listDevices();
+           //readDevice();
+            System.err.println("waiting connect/disconnect...");
+            while(true)
+            {
+                try
+                {
+                    Thread.sleep(1000L);
+                } catch(InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+       }
+       catch(IOException e)
+       {
+          e.printStackTrace();
+       }
+    }
     
     /**
-     * @param args
+     * Static function to read an input report to a HID device.
      */
-    public static void main(String[] args)
-    {
-        listDevices();
-        readDevice();
-    }
-
     private static void readDevice()
     {
         HIDDevice dev;
+        HIDManager hid_mgr;
         try
         {
-            dev = HIDManager.openById(VENDOR_ID, PRODUCT_ID, null);
+            hid_mgr = new HIDManagerTest();
+            dev = hid_mgr.openById(VENDOR_ID, PRODUCT_ID, null);
             System.err.print("Manufacturer: " + dev.getManufacturerString() + "\n");
             System.err.print("Product: " + dev.getProductString() + "\n");
             System.err.print("Serial Number: " + dev.getSerialNumberString() + "\n");
@@ -58,32 +89,45 @@ public class HIDAPITest
                     } catch(InterruptedException e)
                     {
                         //Ignore
+                        e.printStackTrace();
                     }
                 }
             } finally
             {
                 dev.close();
+                hid_mgr.release();    
+                System.gc();
             }
-        } catch(IOException e)
+            
+        } 
+        catch(IOException e)
         {
             e.printStackTrace();
         }
     }
-
+    
+    /**
+     * Static function to find the list of all the HID devices
+     * attached to the system.
+     */
     private static void listDevices()
     {
         String property = System.getProperty("java.library.path");
         System.err.println(property);
-        
+        HIDManager hid_mgr;
         try
         {
-            HIDDeviceInfo[] devs = HIDManager.listDevices();
+            hid_mgr = new HIDManagerTest();
+            
+            HIDDeviceInfo[] devs = hid_mgr.listDevices();
             System.err.println("Devices:\n\n");
             for(int i=0;i<devs.length;i++)
             {
                 System.err.println(""+i+".\t"+devs[i]);
                 System.err.println("---------------------------------------------\n");
             }
+            hid_mgr.release();
+            System.gc();
         }
         catch(IOException e)
         {
